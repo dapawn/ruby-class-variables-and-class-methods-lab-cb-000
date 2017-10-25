bce9eaadc2a667c1b014b70301ff144e37b67b16
David Kuruvilla <dapawn@gmail.com>
2017-10-25 13:13:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,13 @@ class Song
   @@genres = []
   @@artists = []
   @@genre_count = {}
-
+  @@count = 0
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
     @@genres << genre unless @@genres.include?(genre)
     @@artists << artist unless @@artists.include?(artist)
     @@genre_count[genre] ? @@genre_count[genre] += 1 : @@genre_count[genre] = 1
