aa02ef9685cee7d52753bc327a5884aba000206c
David Kuruvilla <dapawn@gmail.com>
2017-10-25 13:08:18 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre unless @@genres.include?(genre)
+    @@artists << atist unless @@artists.include?(artist)
+    @@genre_count[genre] ? @@genre_count[genre] += 1 : @@genre_count[genre] = 1
+  end
+
+  
+end
